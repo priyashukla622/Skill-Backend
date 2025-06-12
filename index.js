559e75e5abcc9ea99cdcfaa22cb1370177992f3e
priya shukla <priyashukla22@navgurukul.org>
2025-06-12 19:41:18 +0530

    added mongodb globle

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import courseRouter from "./routes/route.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT; 
 const mongoURI = process.env.MONGODB_URL;
 mongoose.connect(mongoURI)
 .then(() => {
